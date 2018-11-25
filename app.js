1849392c39746b6106a1ad708a036be1619d290f
root <celalettin@erbulut.com>
2018-11-25 16:09:12 +0300

    Database settings and video time frontend swiper

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var app = express();
+
 var https = require('https');
 //var http = require('http');
 const fs = require('fs');
@@ -8,9 +8,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser')
-var index = require('./routes/index');
 const db = require('./helper/db.js')();
+// ROUTERS
+var swarms = require('./routes/swarm');
+var index = require('./routes/index');
+
+
+//MİDDLEWARE 
 const  video = require("./middleware/video");
+
+var app = express();
+
+
 
 const options = {
   cert: fs.readFileSync("/root/swarm/ssl/certificate.crt"),
@@ -58,18 +67,26 @@ app.use (function (req, res, next) {
   }
 });
 
-app.use(video);
-app.use('/', index);
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 // Set up express server here
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+ 
+
+
+
+app.use(video); 
+app.use('/', index);
+
+app.use("/social",swarms);
+
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,7 @@ router.get('/', function(req, res, next) {
   
   
 });
-router.post('/api/swarm',(req,res,next)=>{
 
-  
-  res.send(req.body)
- 
-   req.io.emit("checkin",req.body.checkin)
- 
-})
 router.post('/video',(req,res,next)=>{
 
   
@@ -32,12 +25,7 @@ router.post('/video',(req,res,next)=>{
           res.json({error:err, code:5})
       })
 })
-router.get('/api',(req,res,next)=>{
 
-  console.log(req.body)
-  res.send(req.body)
-
-})
 
 
 router.get('/video/:video_id', function(req, res) {
@@ -82,35 +70,7 @@ router.get('/video/:video_id', function(req, res) {
       res.json(err);
     })
 
-/*
-  const path = './public/upload/video/demo.mp4';
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
-  const range = req.headers.range
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
-    const end = parts[1] 
-      ? parseInt(parts[1], 10)
-      : fileSize-1
-    const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start, end})
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(206, head);
-    file.pipe(res);
-  } else {
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
-  }*/
+
 });
 
 
